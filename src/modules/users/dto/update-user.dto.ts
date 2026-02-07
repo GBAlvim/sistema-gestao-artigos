6e304b95ea2c1459eeaf0165064a34b5b28e3488
FEODPV473488138 <feodpv473488138@odontoprev.com.br>
2026-02-07 00:41:37 -0300

    chore: deletando arquivos não utilizados

--- src/modules/users/dto/update-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger'; // Use do swagger para manter a doc
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
